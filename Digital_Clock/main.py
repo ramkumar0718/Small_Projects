43961a22734369275ffd65b273277c8f71f22fad
Ramkumar.G <76491314+ramkumar0718@users.noreply.github.com>
2025-02-19 21:57:40 +0530

    Include feature (always-top) and change size

--- Digital_Clock/main.py
+++ Digital_Clock/main.py
@@ -1,18 +1,20 @@
 from tkinter import *
 from tkinter.ttk import *
-
 from time import strftime
 
 root = Tk()
 root.title("Clock")
 root.resizable(0,0)
+root.geometry("235x50")
+# This line is to fix the timer always top
+root.attributes('-topmost', True)
 
 def time():
     string = strftime('%H:%M:%S %p')
     label.config(text=string)
     label.after(1000, time)
 
-label = Label(root, font=("ds-digital", 80), background ="white", foreground = "black")
+label = Label(root, font=("ds-digital", 30), background ="white", foreground = "black")
 label.pack(anchor='center')
 time()
 
